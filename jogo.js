052f5a938bcf8bb7821848fc95724610d0f33a15
1matheusmendes <matheus_mendess@live.com>
2018-04-25 16:49:20 -0300

    finalizando

--- jogo.js
+++ jogo.js
@@ -1,5 +1,47 @@
 function iniciarJogo() {
 	var url = window.location.search;
-	
-	var nivel_jogo = url.replace("?", "")
+
+	var nivel_jogo = url.replace("?", ""); //replace encontra uma caracter para substituir
+
+	//alert(nivel_jogo);
+
+	var tempo_segundos = 0;
+
+	//fácil -> 120 segundos
+	if(nivel_jogo == 1){
+		tempo_segundos = 120;
+	}
+	// normal -> 60 segundos
+	if(nivel_jogo == 2){
+		tempo_segundos = 60;	
+	}
+	//dificil -> 30 segundos
+	if(nivel_jogo == 3){
+		tempo_segundos = 30;
+	}
+
+	//inserindo segundos no span
+	document.getElementById('cronometro').innerHTML = tempo_segundos;
+
+	//quantidade de baloes
+	var qtde_baloes = 20;
+	cria_baloes(qtde_baloes);
+
+	//imprimir balões inteiros
+	document.getElementById('baloes_inteiros').innerHTML = qtde_baloes;
+	document.getElementById('baloes_estourados').innerHTML = 0;
+
+
+}
+
+function cria_baloes(qtde_baloes){
+	for(var i = 1; i <= qtde_baloes; i++){
+		
+		var balao = document.createElement("img");
+		balao.src = 'imagens/balao_azul_pequeno.png';
+		balao.style.margin = '10px';
+
+		document.getElementById('cenario').appendChild(balao);
+
+	}
 }
